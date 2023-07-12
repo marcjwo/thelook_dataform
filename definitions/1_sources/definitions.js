9e59ff8ad9890b7c4c542d256c389dc9d5c55a46
Marc Wollnik <marc@worms.joonix.net>
2023-07-12 16:45:20 +0000

    working

--- definitions/1_sources/definitions.js
+++ definitions/1_sources/definitions.js
@@ -1,16 +1,16 @@
-[
-  "distribution_centers",
-  "users",
-  "events",
-  "inventory_items",
-  "order_items",
-  "products",
-  "orders"
-].forEach((name) =>
-  declare({
-    database: dataform.projectConfig.vars.raw_database, 
-    schema: dataform.projectConfig.vars.raw_dataset,
-    name,
-    tags: ["source"]
-  })
-);
+// [
+//   "distribution_centers",
+//   "users",
+//   "events",
+//   "inventory_items",
+//   "order_items",
+//   "products",
+//   "orders"
+// ].forEach((name) =>
+//   declare({
+//     database: dataform.projectConfig.vars.raw_database, 
+//     schema: dataform.projectConfig.vars.raw_dataset,
+//     name,
+//     tags: ["source"]
+//   })
+// );
--- includes/variables.js
+++ includes/variables.js
@@ -1,9 +1,14 @@
+// Tagging funciton expects 4 args - 1/ table 2/ data owner 3/ data level 4/ data domain
+
+const orders_data_domain = "orders"
+
 // BigQuery Labels
 const source_data_level = "source"
 const intermediate_data_level = "intermediate"
 const output_data_level = "output"
 
 module.exports = {
+    orders_data_domain,
     source_data_level,
     intermediate_data_level,
     output_data_level
